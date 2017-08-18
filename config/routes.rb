4d116bafe02df6885264dfc2949ab7395d7bfc90
tsumi <kdy78022@gmail.com>
2017-08-18 17:26:24 +0900

    productsページの画像にエフェクトを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # match '', to: 'general_pages#index', via: 'get', as: 'root'
 
   match '',          to: 'top#index',    via: 'get', as:'root'
-  match '/love',     to: 'top#love',     via: 'get', as:'love'
+  # match '/love',     to: 'top#love',     via: 'get', as:'love'
   match '/product',  to: 'top#product',  via: 'get', as:'product'
-  match '/activity', to: 'top#activity', via: 'get', as:'activity'
+  # match '/activity', to: 'top#activity', via: 'get', as:'activity'
 
 end
